cac792dc2f5bc1c6a0053db0477f4bf2bb72ce56
Stefano Fornari <stefano.fornari@gmail.com>
2023-07-13 07:41:35 +0200

    Merge branch 'main-upstream' with improvements in newDirectoryStream

--- src/test/java/software/amazon/nio/spi/s3/S3SeekableByteChannelTest.java
+++ src/test/java/software/amazon/nio/spi/s3/S3SeekableByteChannelTest.java
@@ -13,6 +13,7 @@ import software.amazon.awssdk.services.s3.S3AsyncClient;
 import software.amazon.awssdk.services.s3.model.*;
 
 import java.io.IOException;
+import java.net.URI;
 import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.OpenOption;
@@ -21,6 +22,7 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 import java.util.function.Consumer;
+import org.junit.jupiter.api.AfterEach;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
