52119a8dfc003cac1acff5f2a568dc8766a28985
guicamest <guicamest@gmail.com>
2023-09-29 15:19:38 +0200

    rewrite S3ClientProviderTest.java

--- src/test/java/software/amazon/nio/spi/s3/S3ClientProviderTest.java
+++ src/test/java/software/amazon/nio/spi/s3/S3ClientProviderTest.java
@@ -186,11 +186,7 @@ public class S3ClientProviderTest {
         );
 
         // then you should get a NoSuchElement exception when you try to get the header
-        try {
-            provider.generateAsyncClient("test-bucket", mockClient);
-        } catch (Exception e) {
-            assertEquals(NoSuchElementException.class, e.getClass());
-        }
+        assertThrows(NoSuchElementException.class, () -> provider.generateAsyncClient("test-bucket", mockClient));
 
         final InOrder inOrder = inOrder(mockClient);
         inOrder.verify(mockClient).getBucketLocation(any(Consumer.class));
