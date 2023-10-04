7cde748193363e3e9c0ba57d86580ee61a518cf1
guicamest <guicamest@gmail.com>
2023-10-04 04:23:44 +0200

    chore(build): setup integrationtest sourceset

---
+++ src/integrationTest/java/software/amazon/nio/spi/s3/S3FileSystemTest.java
@@ -0,0 +1,21 @@
+package software.amazon.nio.spi.s3;
+
+import org.junit.jupiter.api.Test;
+
+import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static org.assertj.core.api.BDDAssertions.then;
+
+
+public class S3FileSystemTest {
+    @Test
+    public void fileExistsShouldReturnFalseWhenBucketNotFound() {
+        // We expect standard client to fail to locate the bucket
+        final Path path = Paths.get(URI.create("s3://does-not-exists-" + System.currentTimeMillis() + "/dir"));
+        then(path).isInstanceOf(S3Path.class);
+        then(Files.exists(path)).isFalse();
+    }
+}
