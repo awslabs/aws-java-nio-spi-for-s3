171b38972a4e0c4f6ac2e2f414b7a627a7a86653
guicamest <guicamest@gmail.com>
2023-10-04 23:04:34 +0200

    chore(integrationTest): Add more tests for `Files.exists()`

--- src/integrationTest/java/software/amazon/nio/spi/s3/Containers.java
+++ src/integrationTest/java/software/amazon/nio/spi/s3/Containers.java
@@ -1,11 +1,15 @@
 package software.amazon.nio.spi.s3;
 
+import org.testcontainers.containers.Container;
 import org.testcontainers.containers.localstack.LocalStackContainer;
 import org.testcontainers.utility.DockerImageName;
 
+import java.util.concurrent.atomic.AtomicReference;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.testcontainers.containers.localstack.LocalStackContainer.Service.S3;
-import static software.amazon.nio.spi.s3.config.S3NioSpiConfiguration.AWS_REGION_PROPERTY;
-import static software.amazon.nio.spi.s3.config.S3NioSpiConfiguration.AWS_SECRET_ACCESS_KEY_PROPERTY;
+import static software.amazon.nio.spi.s3.config.S3NioSpiConfiguration.S3_SPI_ENDPOINT_PROTOCOL_PROPERTY;
 
 abstract class Containers {
 
@@ -16,13 +20,36 @@ abstract class Containers {
             DockerImageName.parse("localstack/localstack:2.3.2")
         ).withServices(S3);
         LOCAL_STACK_CONTAINER.start();
-
-        System.setProperty("aws.accessKeyId", LOCAL_STACK_CONTAINER.getAccessKey());
-        System.setProperty(AWS_SECRET_ACCESS_KEY_PROPERTY, LOCAL_STACK_CONTAINER.getSecretKey());
-        System.setProperty(AWS_REGION_PROPERTY, LOCAL_STACK_CONTAINER.getRegion());
+        System.setProperty(S3_SPI_ENDPOINT_PROTOCOL_PROPERTY, "http");
     }
 
-    public static String localStackHost() {
+    public static void createBucket(String name) {
+        AtomicReference<Container.ExecResult> execResult = new AtomicReference<>();
+        assertThatCode(() ->
+            execResult.set(LOCAL_STACK_CONTAINER.execInContainer(("awslocal s3api create-bucket --bucket " + name).split(" ")))
+        ).doesNotThrowAnyException();
+        assertThat(execResult.get().getExitCode()).isZero();
+    }
+
+    public static void putObject(String bucket, String key) {
+        AtomicReference<Container.ExecResult> execResult = new AtomicReference<>();
+        assertThatCode(() ->
+            execResult.set(LOCAL_STACK_CONTAINER.execInContainer(("awslocal s3api put-object --bucket "+bucket+" --key "+key).split(" ")))
+        ).doesNotThrowAnyException();
+        assertThat(execResult.get().getExitCode()).isZero();
+    }
+
+    public static String localStackConnectionEndpoint() {
+        return localStackConnectionEndpoint(null, null);
+    }
+
+    public static String localStackConnectionEndpoint(String key, String secret) {
+        String accessKey = key != null ? key : LOCAL_STACK_CONTAINER.getAccessKey();
+        String secretKey = secret != null ? secret : LOCAL_STACK_CONTAINER.getSecretKey();
+        return String.format("s3x://%s:%s@%s", accessKey, secretKey, localStackHost());
+    }
+
+    private static String localStackHost() {
         return LOCAL_STACK_CONTAINER.getEndpoint().getHost() + ":" + LOCAL_STACK_CONTAINER.getEndpoint().getPort();
     }
 }
---
+++ src/integrationTest/java/software/amazon/nio/spi/s3/FilesExistsTest.java
@@ -0,0 +1,65 @@
+package software.amazon.nio.spi.s3;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static org.assertj.core.api.BDDAssertions.then;
+import static software.amazon.nio.spi.s3.Containers.localStackConnectionEndpoint;
+import static software.amazon.nio.spi.s3.Containers.putObject;
+
+@DisplayName("Files$exists()")
+public class FilesExistsTest {
+
+    @Nested
+    @DisplayName("should be false")
+    class FilesDoesNotExist {
+
+        @Test
+        @DisplayName("when bucket does not exist")
+        public void fileExistsShouldReturnFalseWhenBucketNotFound() {
+            // We expect standard client to fail to locate the bucket
+            final Path path = Paths.get(URI.create(localStackConnectionEndpoint() + "/does-not-exist"));
+            then(Files.exists(path)).isFalse();
+        }
+
+        @Test
+        @DisplayName("when bucket exists but file doesn't")
+        public void fileExistsShouldReturnFalseWhenBucketExistsAndFileNotFound() {
+            Containers.createBucket("sink");
+            final Path path = Paths.get(URI.create(localStackConnectionEndpoint() + "/sink/missing-file"));
+            then(Files.exists(path)).isFalse();
+        }
+    }
+
+    @Nested
+    @DisplayName("should be true")
+    class FilesExists {
+
+        @BeforeEach
+        public void createBucket() {
+            Containers.createBucket("sink");
+        }
+
+        @Test
+        @DisplayName("when bucket and file exist")
+        public void fileExistsShouldReturnTrueWhenBucketExistsAndFileFound() {
+            putObject("sink", "sample-file.txt");
+            final Path path = Paths.get(URI.create(localStackConnectionEndpoint() + "/sink/sample-file.txt"));
+            then(Files.exists(path)).isTrue();
+        }
+
+        @Test
+        @DisplayName("for bucket path when it exists")
+        public void fileExistsShouldReturnTrueWhenBucketExists() {
+            final Path path = Paths.get(URI.create(localStackConnectionEndpoint() + "/sink/"));
+            then(Files.exists(path)).isTrue();
+        }
+    }
+}
