068ce07b342f83322f323b484e221c5cd3c69640
Mark Schreiber <markjschreiber@gmail.com>
2023-11-03 11:03:10 -0400

    Markjschreiber/example update (#261)
    * * Extracts logic from long method
    * Ensures use of try with resource for autoclosable filesystem.
    * Improve exception handling
    * Javadoc improvement
    
    * Avoid unnecessary creation of a new Filesystem

--- src/examples/java/software/amazon/nio/spi/examples/ListPrefix.java
+++ src/examples/java/software/amazon/nio/spi/examples/ListPrefix.java
@@ -2,11 +2,8 @@ package software.amazon.nio.spi.examples;
 
 import java.io.IOException;
 import java.net.URI;
-import java.nio.file.FileSystem;
-import java.nio.file.FileSystems;
 import java.nio.file.Path;
-
-import static java.util.Collections.*;
+import java.nio.file.Paths;
 
 public class ListPrefix {
     public static void main(String[] args) throws IOException {
@@ -16,15 +13,14 @@ public class ListPrefix {
         }
 
         String prefix = args[0];
-        try (final FileSystem fileSystem = FileSystems.newFileSystem(URI.create(prefix), emptyMap())) {
-            Path s3Path = fileSystem.getPath(prefix);
 
-            assert fileSystem.getClass().getName().contains("S3FileSystem");
-            assert s3Path.getClass().getName().contains("S3Path");
+        Path s3Path = Paths.get(URI.create(prefix));
 
-            fileSystem.provider()
-                    .newDirectoryStream(s3Path, item -> true)
-                    .forEach(path -> System.out.println(path.getFileName()));
-        }
+        assert s3Path.getClass().getName().contains("S3Path");
+
+        s3Path.getFileSystem().provider()
+                .newDirectoryStream(s3Path, item -> true)
+                .forEach(path -> System.out.println(path.getFileName()));
+
     }
 }
