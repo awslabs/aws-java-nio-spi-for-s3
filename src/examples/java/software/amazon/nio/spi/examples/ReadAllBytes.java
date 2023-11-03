9cf078bf891417482d0d96411288d59c6f84a1a6
Mark Schreiber <mrschre@amazon.com>
2023-11-03 10:38:10 -0400

    Added example

--- src/examples/java/software/amazon/nio/spi/examples/ReadAllBytes.java
+++ src/examples/java/software/amazon/nio/spi/examples/ReadAllBytes.java
@@ -1,2 +1,19 @@
-package software.amazon.nio.spi.examples;public class ReadAllBytes {
+package software.amazon.nio.spi.examples;
+
+import java.io.IOException;
+import java.net.URI;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class ReadAllBytes {
+    public static void main(String[] args) throws IOException {
+        Path filePath = Paths.get(URI.create(args[0]));
+
+        final byte[] bytes = Files.readAllBytes(filePath);
+        // assumes this is a text file
+        final String data = new String(bytes, StandardCharsets.UTF_8);
+        System.out.println(data);
+    }
 }
