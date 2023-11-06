2af99137be558e2f7ed99ea86d647b85c14f1de5
Mark Schreiber <markjschreiber@gmail.com>
2023-11-06 17:05:16 -0500

    Refactor to use Files.list() (#275)

--- src/examples/java/software/amazon/nio/spi/examples/ListPrefix.java
+++ src/examples/java/software/amazon/nio/spi/examples/ListPrefix.java
@@ -2,8 +2,10 @@ package software.amazon.nio.spi.examples;
 
 import java.io.IOException;
 import java.net.URI;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.stream.Stream;
 
 public class ListPrefix {
     public static void main(String[] args) throws IOException {
@@ -12,15 +14,10 @@ public class ListPrefix {
             System.exit(1);
         }
 
-        String prefix = args[0];
+        Path s3Path = Paths.get(URI.create(args[0]));
 
-        Path s3Path = Paths.get(URI.create(prefix));
-
-        assert s3Path.getClass().getName().contains("S3Path");
-
-        s3Path.getFileSystem().provider()
-                .newDirectoryStream(s3Path, item -> true)
-                .forEach(path -> System.out.println(path.getFileName()));
-
+        try (Stream<Path> listed = Files.list(s3Path)) {
+            listed.forEach(System.out::println);
+        }
     }
 }
