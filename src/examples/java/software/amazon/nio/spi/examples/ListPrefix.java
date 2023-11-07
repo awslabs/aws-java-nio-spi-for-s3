43da361999970aa0e3a3bf7d6e9b4293610e577e
Mark Schreiber <mrschre@amazon.com>
2023-11-07 16:08:58 -0500

    fix: use system err for messages

--- src/examples/java/software/amazon/nio/spi/examples/ListPrefix.java
+++ src/examples/java/software/amazon/nio/spi/examples/ListPrefix.java
@@ -10,11 +10,17 @@ import java.util.stream.Stream;
 public class ListPrefix {
     public static void main(String[] args) throws IOException {
         if (args.length == 0) {
-            System.out.println("Provide an s3 prefix to list.");
+            System.err.println("Provide an s3 prefix to list.");
             System.exit(1);
         }
 
         Path s3Path = Paths.get(URI.create(args[0]));
+
+        // if the prefix doesn't exist you won't get any listings
+        System.err.println("Files.exists(s3Path) = " + Files.exists(s3Path));
+
+        //if it's not a directory you won't get any listings.
+        System.err.println("Files.isDirectory(s3Path) = " + Files.isDirectory(s3Path));
 
         try (Stream<Path> listed = Files.list(s3Path)) {
             listed.forEach(System.out::println);
