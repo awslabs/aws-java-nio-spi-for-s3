28bd6e83fb23ecc5e43aac4375c63717d61c967a
guicamest <guicamest@gmail.com>
2023-11-08 16:26:24 +0100

    test(S3WritableByteChannelTest): `when file does not exist and constructor is invoked without option `CREATE_NEW` nor `CREATE` should throw NoSuchFileException`

--- src/test/java/software/amazon/nio/spi/s3/S3WritableByteChannelTest.java
+++ src/test/java/software/amazon/nio/spi/s3/S3WritableByteChannelTest.java
@@ -4,11 +4,13 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.nio.file.FileAlreadyExistsException;
+import java.nio.file.NoSuchFileException;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
 import static java.nio.file.StandardOpenOption.CREATE_NEW;
+import static java.util.Collections.emptySet;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
@@ -28,5 +30,19 @@ class S3WritableByteChannelTest {
         S3Path file = S3Path.getPath(fs, "somefile");
         assertThatThrownBy(() -> new S3WritableByteChannel(file, mock(), Set.of(CREATE_NEW), 0L, TimeUnit.MINUTES))
                 .isInstanceOf(FileAlreadyExistsException.class);
+    }
+
+    @Test
+    @DisplayName("when file does not exist and constructor is invoked without option `CREATE_NEW` nor `CREATE` should throw NoSuchFileException")
+    void whenFileDoesNotExistsAndNoCreateNewShouldThrowNoSuchFileException() throws InterruptedException, TimeoutException {
+        S3FileSystemProvider provider = mock();
+        when(provider.exists(any(), any())).thenReturn(false);
+
+        S3FileSystem fs = mock();
+        when(fs.provider()).thenReturn(provider);
+
+        S3Path file = S3Path.getPath(fs, "somefile");
+        assertThatThrownBy(() -> new S3WritableByteChannel(file, mock(), emptySet(), 0L, TimeUnit.MINUTES))
+                .isInstanceOf(NoSuchFileException.class);
     }
 }
