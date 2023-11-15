eef44b1709a03047b75bb3e3da9b91d264a201d2
guicamest <guicamest@gmail.com>
2023-11-15 23:46:09 +0100

    checkstyle(S3TransferUtil): Reformat

--- src/main/java/software/amazon/nio/spi/s3/S3TransferUtil.java
+++ src/main/java/software/amazon/nio/spi/s3/S3TransferUtil.java
@@ -5,19 +5,18 @@
 
 package software.amazon.nio.spi.s3;
 
-import software.amazon.awssdk.services.s3.S3AsyncClient;
-import software.amazon.awssdk.services.s3.model.GetObjectRequest;
-import software.amazon.awssdk.services.s3.model.PutObjectRequest;
-import software.amazon.awssdk.transfer.s3.S3TransferManager;
-import software.amazon.awssdk.transfer.s3.model.DownloadFileRequest;
-import software.amazon.awssdk.transfer.s3.model.UploadFileRequest;
-
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
+import software.amazon.awssdk.services.s3.S3AsyncClient;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.transfer.s3.S3TransferManager;
+import software.amazon.awssdk.transfer.s3.model.DownloadFileRequest;
+import software.amazon.awssdk.transfer.s3.model.UploadFileRequest;
 
 final class S3TransferUtil {
     private final S3AsyncClient client;
@@ -33,13 +32,13 @@ final class S3TransferUtil {
     void downloadToLocalFile(S3Path path, Path destination) throws InterruptedException, ExecutionException, TimeoutException {
         try (var s3TransferManager = S3TransferManager.builder().s3Client(client).build()) {
             var downloadCompletableFuture = s3TransferManager.downloadFile(
-                    DownloadFileRequest.builder()
-                            .getObjectRequest(GetObjectRequest.builder()
-                                    .bucket(path.bucketName())
-                                    .key(path.getKey())
-                                    .build())
-                            .destination(destination)
-                            .build()
+                DownloadFileRequest.builder()
+                    .getObjectRequest(GetObjectRequest.builder()
+                        .bucket(path.bucketName())
+                        .key(path.getKey())
+                        .build())
+                    .destination(destination)
+                    .build()
             ).completionFuture();
 
             if (timeout != null && timeUnit != null) {
@@ -53,14 +52,14 @@ final class S3TransferUtil {
     void uploadLocalFile(S3Path path, Path localFile) throws IOException {
         try (var s3TransferManager = S3TransferManager.builder().s3Client(client).build()) {
             var uploadCompletableFuture = s3TransferManager.uploadFile(
-                    UploadFileRequest.builder()
-                            .putObjectRequest(PutObjectRequest.builder()
-                                    .bucket(path.bucketName())
-                                    .key(path.getKey())
-                                    .contentType(Files.probeContentType(localFile))
-                                    .build())
-                            .source(localFile)
-                            .build()
+                UploadFileRequest.builder()
+                    .putObjectRequest(PutObjectRequest.builder()
+                        .bucket(path.bucketName())
+                        .key(path.getKey())
+                        .contentType(Files.probeContentType(localFile))
+                        .build())
+                    .source(localFile)
+                    .build()
             ).completionFuture();
 
             if (timeout != null && timeUnit != null) {
