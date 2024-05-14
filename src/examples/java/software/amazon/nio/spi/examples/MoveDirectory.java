7182f46a1a28688fffb3a98ba74005860e713fad
Mark Schreiber <mrschre@amazon.com>
2024-05-14 14:37:35 -0400

    fix: resolves issues when copying or moving objects via the `Files` class

--- src/examples/java/software/amazon/nio/spi/examples/MoveDirectory.java
+++ src/examples/java/software/amazon/nio/spi/examples/MoveDirectory.java
@@ -14,43 +14,36 @@ import java.nio.file.Path;
  * Demonstrates a move operation using the `Files` class
  */
 @SuppressWarnings("CheckStyle")
-public class MoveObject {
+public class MoveDirectory {
 
 
     public static void main(String[] args) throws IOException {
 
         if (args.length != 2) {
-            System.err.println("Usage: java MoveObject <source> <destination>");
+            System.err.println("Usage: java MoveObject <sourceDir> <destinationDir>");
             System.exit(1);
         }
-
-        System.err.printf("Moving %s to %s%n", args[0], args[1]);
 
         final URI source = URI.create(args[0]);
         final URI destination = URI.create(args[1]);
 
         final Path destinationPath = Path.of(destination);
         final Path sourcePath = Path.of(source);
+
+        assert Files.isDirectory(sourcePath);
+        assert Files.isDirectory(destinationPath);
+
         System.err.println("Source path: " + sourcePath);
         System.err.println("Destination path: " + destinationPath);
 
+        System.err.printf("Moving %s to %s%n", args[0], args[1]);
 
-        final long sizeOfSource = Files.size(sourcePath);
         Path movedTo = Files.move(sourcePath, destinationPath);
-        if (Files.isDirectory(movedTo)) {
-            movedTo = movedTo.resolve(sourcePath.getFileName());
-        }
 
         System.err.printf("Moved %s to %s%n", sourcePath, movedTo);
-        final long sizeOfDestination = Files.size(movedTo);
-
-        assert Files.exists(movedTo);
-        assert sizeOfSource == sizeOfDestination;
 
         // move it back
         Files.move(movedTo, sourcePath);
         System.err.printf("Moved %s back to %s%n", movedTo, sourcePath);
-        assert Files.exists(sourcePath);
-        assert sizeOfSource == Files.size(sourcePath);
     }
 }
