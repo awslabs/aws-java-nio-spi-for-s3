7073819e20b9c4f64480e91424d4cc91fad0fe2c
Mark Schreiber <mrschre@amazon.com>
2024-12-02 12:02:17 -0500

    delegates cross region bucket location determination to SDK

--- src/test/java/software/amazon/nio/spi/s3/S3ClientProviderTest.java
+++ src/test/java/software/amazon/nio/spi/s3/S3ClientProviderTest.java
@@ -9,33 +9,20 @@ import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNotSame;
 import static org.junit.jupiter.api.Assertions.assertSame;
-import static org.mockito.Mockito.inOrder;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-import static software.amazon.nio.spi.s3.S3Matchers.anyConsumer;
 
 import java.net.URI;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import software.amazon.awssdk.services.s3.S3AsyncClient;
-import software.amazon.awssdk.services.s3.model.GetBucketLocationResponse;
-import software.amazon.awssdk.services.s3.model.HeadBucketResponse;
-import software.amazon.awssdk.services.s3.model.S3Exception;
 import software.amazon.nio.spi.s3.config.S3NioSpiConfiguration;
 
 @ExtendWith(MockitoExtension.class)
 public class S3ClientProviderTest {
-
-    @Mock
-    S3AsyncClient mockClient; //client used to determine bucket location
 
     S3ClientProvider provider;
 
@@ -50,85 +37,44 @@ public class S3ClientProviderTest {
 
         assertNotNull(s3ClientProvider.configuration);
 
-        S3AsyncClient t = s3ClientProvider.universalClient();
-        assertNotNull(t);
-
         var config = new S3NioSpiConfiguration();
         assertSame(config, new S3ClientProvider(config).configuration);
     }
 
     @Test
-    public void testGenerateAsyncClientWithNoErrors() throws ExecutionException, InterruptedException {
-        when(mockClient.headBucket(anyConsumer()))
-                .thenReturn(CompletableFuture.completedFuture(
-                        HeadBucketResponse.builder().bucketRegion("us-west-2").build()));
-        final var s3Client = provider.generateClient("test-bucket", mockClient);
+    public void testGenerateAsyncClientWithNoErrors() {
+        final var s3Client = provider.generateClient("test-bucket");
         assertNotNull(s3Client);
     }
 
     @Test
-    public void testGenerateClientIsCacheableClass() throws Exception {
-        when(mockClient.headBucket(anyConsumer()))
-                .thenReturn(CompletableFuture.completedFuture(
-                        HeadBucketResponse.builder().bucketRegion("us-west-2").build()));
-        final var s3Client = provider.generateClient("test-bucket", mockClient);
+    public void testGenerateClientIsCacheableClass() {
+        final var s3Client = provider.generateClient("test-bucket");
         assertInstanceOf(CacheableS3Client.class, s3Client);
     }
 
     @Test
-    public void testGenerateClientCachesClients() throws Exception {
-        when(mockClient.headBucket(anyConsumer()))
-                .thenReturn(CompletableFuture.completedFuture(
-                        HeadBucketResponse.builder().bucketRegion("us-west-2").build()));
-        final var s3Client = provider.generateClient("test-bucket", mockClient);
-        final var s3Client2 = provider.generateClient("test-bucket", mockClient);
+    public void testGenerateClientCachesClients() {
+        final var s3Client = provider.generateClient("test-bucket");
+        final var s3Client2 = provider.generateClient("test-bucket");
         assertSame(s3Client, s3Client2);
     }
 
     @Test
-    public void testClosedClientIsNotReused() throws ExecutionException, InterruptedException {
-        when(mockClient.headBucket(anyConsumer()))
-                .thenReturn(CompletableFuture.completedFuture(
-                        HeadBucketResponse.builder().bucketRegion("us-west-2").build()));
+    public void testClosedClientIsNotReused() {
 
-        final var s3Client = provider.generateClient("test-bucket", mockClient);
+        final var s3Client = provider.generateClient("test-bucket");
         assertNotNull(s3Client);
 
         // now close the client
         s3Client.close();
 
         // now generate a new client with the same bucket name
-        final var s3Client2 = provider.generateClient("test-bucket", mockClient);
+        final var s3Client2 = provider.generateClient("test-bucket");
         assertNotNull(s3Client2);
 
         // assert it is not the closed client
         assertNotSame(s3Client, s3Client2);
-    }
-
-    @Test
-    public void testGenerateAsyncClientWith403Response() throws ExecutionException, InterruptedException {
-        // when you get a forbidden response from HeadBucket
-        when(mockClient.headBucket(anyConsumer())).thenReturn(
-                CompletableFuture.failedFuture(S3Exception.builder().statusCode(403).build())
-        );
-
-        // you should fall back to a get bucket location attempt from the universal client
-        var mockUniversalClient = mock(S3AsyncClient.class);
-        provider.universalClient(mockUniversalClient);
-        when(mockUniversalClient.getBucketLocation(anyConsumer())).thenReturn(CompletableFuture.completedFuture(
-                GetBucketLocationResponse.builder()
-                        .locationConstraint("us-west-2")
-                        .build()
-        ));
-
-        // which should get you a client
-        final var s3Client = provider.generateClient("test-bucket", mockClient);
-        assertNotNull(s3Client);
-
-        final var inOrder = inOrder(mockClient, mockUniversalClient);
-        inOrder.verify(mockClient).headBucket(anyConsumer());
-        inOrder.verify(mockUniversalClient).getBucketLocation(anyConsumer());
-        inOrder.verifyNoMoreInteractions();
     }
 
     @Test
@@ -144,7 +90,6 @@ public class S3ClientProviderTest {
 
         // THEN
         verify(BUILDER, times(1)).endpointOverride(URI.create("https://endpoint1:1010"));
-        verify(BUILDER, times(1)).region(null);
 
         // GIVEN
         BUILDER = spy(S3AsyncClient.crtBuilder());
@@ -156,6 +101,5 @@ public class S3ClientProviderTest {
 
         // THEN
         verify(BUILDER, times(1)).endpointOverride(URI.create("https://endpoint2:2020"));
-        verify(BUILDER, times(1)).region(null);
     }
 }
