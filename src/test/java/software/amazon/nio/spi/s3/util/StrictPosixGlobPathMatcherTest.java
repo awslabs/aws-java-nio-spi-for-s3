0081f25113e0f96682142643dec3b99da463ce26
Mark Schreiber <mrschre@amazon.com>
2025-04-14 17:12:47 -0400

    Add comprehensive tests for StrictPosixGlobPathMatcher to improve coverage

--- src/test/java/software/amazon/nio/spi/s3/util/StrictPosixGlobPathMatcherTest.java
+++ src/test/java/software/amazon/nio/spi/s3/util/StrictPosixGlobPathMatcherTest.java
@@ -25,8 +25,12 @@ class StrictPosixGlobPathMatcherTest {
 
     @Test
     void testDirectoryGlobMatching() {
-        // For now, we'll skip this test as it requires more debugging
-        // The implementation works correctly in manual testing
+        // Test directory matching with single asterisk
+        StrictPosixGlobPathMatcher matcher = new StrictPosixGlobPathMatcher("dir/*/file.txt");
+        
+        assertThat(matcher.matches(Paths.get("dir/subdir/file.txt"))).isTrue();
+        assertThat(matcher.matches(Paths.get("dir/file.txt"))).isFalse();
+        assertThat(matcher.matches(Paths.get("dir/subdir/subsubdir/file.txt"))).isFalse();
     }
 
     @Test
@@ -65,5 +69,64 @@ class StrictPosixGlobPathMatcherTest {
         
         assertThat(matcher.matches(Paths.get("file*.txt"))).isTrue();
         assertThat(matcher.matches(Paths.get("fileA.txt"))).isFalse();
+    }
+    
+    @Test
+    void testComplexCharacterClassMatching() {
+        // Test character class with range
+        StrictPosixGlobPathMatcher matcher = new StrictPosixGlobPathMatcher("file[A-Z].txt");
+        
+        assertThat(matcher.matches(Paths.get("fileA.txt"))).isTrue();
+        assertThat(matcher.matches(Paths.get("fileZ.txt"))).isTrue();
+        assertThat(matcher.matches(Paths.get("file1.txt"))).isFalse();
+        
+        // Test character class with individual characters
+        StrictPosixGlobPathMatcher matcher2 = new StrictPosixGlobPathMatcher("file[xyz].txt");
+        
+        assertThat(matcher2.matches(Paths.get("filex.txt"))).isTrue();
+        assertThat(matcher2.matches(Paths.get("filey.txt"))).isTrue();
+        assertThat(matcher2.matches(Paths.get("filez.txt"))).isTrue();
+        assertThat(matcher2.matches(Paths.get("filea.txt"))).isFalse();
+    }
+    
+    @Test
+    void testSpecialCharactersInPattern() {
+        // Test dot character (should be treated as literal)
+        StrictPosixGlobPathMatcher matcher = new StrictPosixGlobPathMatcher("file.txt");
+        
+        assertThat(matcher.matches(Paths.get("file.txt"))).isTrue();
+        assertThat(matcher.matches(Paths.get("filetxt"))).isFalse();
+        assertThat(matcher.matches(Paths.get("fileAtxt"))).isFalse();
+        
+        // Test with parentheses (should be treated as literals)
+        StrictPosixGlobPathMatcher matcher2 = new StrictPosixGlobPathMatcher("file(test).txt");
+        
+        assertThat(matcher2.matches(Paths.get("file(test).txt"))).isTrue();
+        assertThat(matcher2.matches(Paths.get("filetest.txt"))).isFalse();
+        
+        // Test with square brackets (should be treated as character class)
+        StrictPosixGlobPathMatcher matcher3 = new StrictPosixGlobPathMatcher("file\\[test\\].txt");
+        
+        assertThat(matcher3.matches(Paths.get("file[test].txt"))).isTrue();
+        assertThat(matcher3.matches(Paths.get("filet.txt"))).isFalse();
+        
+        // Test with other regex special characters
+        StrictPosixGlobPathMatcher matcher4 = new StrictPosixGlobPathMatcher("file+^$|().txt");
+        
+        assertThat(matcher4.matches(Paths.get("file+^$|().txt"))).isTrue();
+        assertThat(matcher4.matches(Paths.get("file.txt"))).isFalse();
+    }
+    
+    @Test
+    void testGetPattern() {
+        String pattern = "*.txt";
+        StrictPosixGlobPathMatcher matcher = new StrictPosixGlobPathMatcher(pattern);
+        
+        assertThat(matcher.getPattern()).isEqualTo(pattern);
+        
+        String complexPattern = "dir/**/[a-z]file?.txt";
+        StrictPosixGlobPathMatcher complexMatcher = new StrictPosixGlobPathMatcher(complexPattern);
+        
+        assertThat(complexMatcher.getPattern()).isEqualTo(complexPattern);
     }
 }
